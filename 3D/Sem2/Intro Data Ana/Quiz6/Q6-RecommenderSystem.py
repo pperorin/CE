e7580dd298331873a169916bccb43e4e3c1ca4b6
pperorin <zentoa@hotmail.co.th>
2022-03-24 17:38:30 +0700

    Lab8 Soft Arch

---
+++ 3D/Sem2/Intro Data Ana/Quiz6/Q6-RecommenderSystem.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("data.csv")
+# f=open("data.xlsx","r")
+# print(f.read())
+# df = pd.read_excel(f)
+print(df)
